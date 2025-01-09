3a4302697800b5f108aa912001e1f08c5cb936a7
Des3uct0r <sviatpukas21@gmail.com>
2025-01-09 08:03:52 +0200

    dz nbu gotovo

--- dz$.py
+++ dz$.py
@@ -1,17 +1,18 @@
 import requests
 
 
-def get_rate(aud):
-    aud_amount = float(input("Enter the amount of AUD: "))
+def get_rate():
+    aud_amount = float(input("Enter how many  you need: "))
     url = 'https://bank.gov.ua/ua/markets/exchangerates'
-    r = requests.get(url)
-    t = r.text.split("<td")
+    rp = requests.get(url)
+    t = rp.text.split("<td")
     rates = []
     for p in t:
         if 'data-label="Офіційний курс"' in p:
-            rates.append(p.split(">")[1].split("<")[0])
-    return f"UAH to AUD : {rates[0]} UAH per {aud_amount} AUD."
-
+            rates.append(p.split(">")[1].split("<")[0].replace(',','.'))
+    rate_of_aud = float(rates[0])
+    res = aud_amount * rate_of_aud
+    return f"{res} UAH for {aud_amount} AUD."
 
 
 
